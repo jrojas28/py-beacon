75857ba346d1b28493668abcc6b3834f5ebdfaf7
Jaime Rojas <j_rojas94@hotmail.com>
2018-04-21 15:56:18 -0400

    Implemented Collector Socket Version

--- collector.py
+++ collector.py
@@ -2,38 +2,40 @@
 # by Taka Wang
 #
 
+import socket
 import ConfigParser
-import paho.mqtt.client as mqtt
 from proximity import *
 
 DEBUG = True
 
-def onConnect(client, userdata, rc):
-    """MQTT onConnect handler"""
-    print("Connected to broker: " + str(rc))
+def sendData(socket, target, data):
+    socket.sendTo(data, (target))
 
-def initMQTT(url = "localhost", port = 1883, keepalive = 60):
-    """Init MQTT connection"""
-    client = mqtt.Client()
-    client.on_connect = onConnect
+def receiveData(socket):
+    data, addr = socket.recvFrom(1024)
+    return (data, addr)
+
+def initSocket(receiveAddress = ('', 9876)):
+    """Init Socket Listener connection"""
     try:
-        client.connect(url, port, keepalive)
-        client.loop_start()
-        return client
+        udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udpSocket.bind(receiveAddress)
+        return udpSocket
     except Exception, e:
         print(e)
         return None
 
-def startScan(mqttclnt, filter="", topic="/ble/rssi/"):
-    """Scan BLE beacon and publish to MQTT broker"""
-    if mqttclnt:
+def startScan(socket, sendAddress = ('127.0.0.1', 9870), filter="", topic="/ble/rssi/"):
+    """Scan BLE beacon and publish to Socket Server"""
+    if socket:
         scanner = Scanner()
         while True:
             for beacon in scanner.scan():
                 fields = beacon.split(",")
                 if fields[1].startswith(filter):
-                    mqttclnt.publish(topic, '{"id":"%s","val":"%s"}' % (fields[0], fields[5]))
-                    if DEBUG: print(fields[0], fields[5])
+                    sendData(socket, sendAddress, '{"id":"%s","val":"%s"}' % (fields[0], fields[5]))
+                    if DEBUG: 
+                        print('{"id":"%s","val":"%s"}' % (fields[0], fields[5]))
 
 def init():
     """Read config file"""
@@ -42,14 +44,16 @@ def init():
     config.read("config")
     global DEBUG
     DEBUG = True if int(config.get('Collector', 'debug')) == 1 else False
-    ret["url"]       = config.get('MQTT', 'url')
-    ret["port"]      = int(config.get('MQTT', 'port'))
-    ret["keepalive"] = int(config.get('MQTT', 'keepalive'))
-    ret["filter"]    = config.get('Scanner', 'filter')
-    ret["topic_id"]  = config.get('Scanner', 'topic_id')
+    ret["send_url"]         = config.get('SOCKET', 'send_url')
+    ret["send_port"]        = int(config.get('SOCKET', 'send_port'))
+    ret["receive_url"]      = config.get('SOCKET', 'receive_url')
+    ret["receive_port"]     = int(config.get('SOCKET', 'receive_port'))
+    ret["filter"]           = config.get('Scanner', 'filter')
+    ret["topic_id"]         = config.get('Scanner', 'topic_id')
     return ret
 
 if __name__ == '__main__':
     conf = init()
-    clnt = initMQTT(conf["url"], conf["port"], conf["keepalive"])
-    startScan(clnt, conf["filter"], conf["topic_id"])
+    socket = initSocket((conf["receive_url"], conf["receive_url"]))
+    sendAddress = (conf["send_url"], conf["send_port"])
+    startScan(socket, sendAddress, conf["filter"], conf["topic_id"])
